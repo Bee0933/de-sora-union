d397e6c7edca24fc0626f0fb5b350daec5b11de4
Bee0933 <bestnyah7@gmail.com>
2024-11-16 14:20:09 +0100

    wip: mod airflow
    Signed-off-by: Bee0933 <bestnyah7@gmail.com>

--- airflow/dags/etl_process_clickup_data.py
+++ airflow/dags/etl_process_clickup_data.py
@@ -1,108 +1,62 @@
+import sys, os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 from datetime import datetime
-from sqlalchemy import create_engine
-import pandas as pd
-import logging
 
-# Logging config
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler()],
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pipelines.sheets_to_pg import (
+    download_google_sheet,
+    transform_clickup_data,
+    ingest_to_postgres,
 )
 
-# Google Sheet ID and sheet name
+
+# Variables
 SHEET_ID = Variable.get("CLICKUP_SHEET_ID")
 SHEET_NAME = Variable.get("CLICKUP_SHEET_NAME")
 TABLE_NAME = Variable.get("CLICKUP_DWH_TABLE_NAME")
+HOST = Variable.get("POSTGRES_HOST")
+PORT = Variable.get("POSTGRES_PORT")
+USER = Variable.get("POSTGRES_USER")
+PASSWORD = Variable.get("POSTGRES_PASSWORD")
+DATABASE = Variable.get("POSTGRES_DB")
 
 # connection URL for public Google Sheet as CSV
-CSV_URL = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
-
-
-def download_google_sheet(**kwargs):
-    try:
-        df = pd.read_csv(CSV_URL)
-        kwargs['ti'].xcom_push(key='sheet_data', value=df.to_dict())
-        logging.info("Google Sheet data downloaded and pushed to XCom")
-    except Exception as e:
-        logging.error(f'An error occured: {e}')
-
-def transform_data(**kwargs):
-    try:
-        ti = kwargs['ti']
-        sheet_data = ti.xcom_pull(key='sheet_data', task_ids='download_google_sheet')
-        df = pd.DataFrame(sheet_data)
-        
-        # transform data
-        df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
-        string_columns = ['client', 'project', 'name', 'task', 'note']
-        for col in string_columns:
-            if col in df.columns:
-                df[col] = df[col].str.strip().str.title()
-        if 'date' in df.columns:
-            df['date'] = pd.to_datetime(df['date'], errors='coerce')
-        if 'hours' in df.columns:
-            df['hours'] = pd.to_numeric(df['hours'], errors='coerce')
-        if 'billable' in df.columns:
-            df['billable'] = df['billable'].str.strip().str.lower().map({'yes': True, 'no': False})
-        critical_columns = ['client', 'project', 'name', 'task', 'date', 'hours']
-        df = df.dropna(subset=critical_columns)
-        if 'note' in df.columns:
-            df['note'] = df['note'].fillna('No Note')
-        df = df.drop_duplicates()
-
-        logging.info("Transformed DataFrame:")
-        logging.info(df.head())
-        kwargs['ti'].xcom_push(key='transformed_data', value=df.to_dict())
-    except Exception as e:
-        logging.error(f'An error occured: {e}')
-
-def ingest_to_postgres(**kwargs):
-    try:
-        ti = kwargs['ti']
-        transformed_data = ti.xcom_pull(key='transformed_data', task_ids='transform_data')
-        df_transformed = pd.DataFrame(transformed_data)
-
-        host = Variable.get("POSTGRES_HOST")
-        port = Variable.get("POSTGRES_PORT")
-        user = Variable.get("POSTGRES_USER")
-        password = Variable.get("POSTGRES_PASSWORD")
-        database = Variable.get("POSTGRES_DB")
-
-        engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}')
-        df_transformed.to_sql(TABLE_NAME, con=engine, if_exists='append', index=False)
-        logging.info("Transformed data successfully ingested into PostgreSQL using pandas")
-    except Exception as e:
-        logging.error(f'An error occured: {e}')
-
+CSV_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
 
 # Define the DAG
 with DAG(
-    'CLICKUP_DATA_TO_PG',
+    "CLICKUP_DATA_TO_PG",
     start_date=datetime(2024, 11, 15),
-    schedule_interval='@daily',
+    schedule_interval="@daily",
     catchup=False,
 ) as dag:
 
     download_sheet_task = PythonOperator(
-        task_id='download_google_sheet',
-        python_callable=download_google_sheet,
-        provide_context=True
+        task_id="download_clickup_sheet",
+        python_callable=lambda: download_google_sheet(CSV_URL),
     )
 
     transform_task = PythonOperator(
-        task_id='transform_data',
-        python_callable=transform_data,
-        provide_context=True  
+        task_id="transform_clickup_data",
+        python_callable=lambda ti: transform_clickup_data(
+            ti.xcom_pull(task_ids="download_clickup_sheet")
+        ),
     )
 
     ingest_to_postgres_task = PythonOperator(
-        task_id='ingest_to_postgres',
-        python_callable=ingest_to_postgres,
-        provide_context=True  
+        task_id="ingest_clickup_data_to_postgres",
+        python_callable=lambda ti: ingest_to_postgres(
+            ti.xcom_pull(task_ids="transform_clickup_data"),
+            USER,
+            PASSWORD,
+            HOST,
+            PORT,
+            DATABASE,
+            TABLE_NAME,
+        ),
     )
 
     # task order order
---
+++ airflow/pipelines/sheets_to_pg.py
@@ -0,0 +1,106 @@
+import logging
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Logging config
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+
+def download_google_sheet(csv_url: str):
+    try:
+        df = pd.read_csv(csv_url)
+        logging.info("Google Sheet data downloaded.")
+        return df.to_dict()
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise
+
+
+def ingest_to_postgres(
+    transformed_data,
+    user: str,
+    password: str,
+    host: str,
+    port: str,
+    database: str,
+    table_name: str,
+):
+    try:
+        df_transformed = pd.DataFrame(transformed_data)
+
+        engine = create_engine(
+            f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+        )
+        df_transformed.to_sql(table_name, con=engine, if_exists="append", index=False)
+        logging.info(
+            "Transformed data successfully ingested into PostgreSQL using pandas"
+        )
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise
+
+
+def transform_float_data(sheet_data):
+    try:
+        df = pd.DataFrame(sheet_data)
+
+        # transform data
+        df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+        df["estimated_hours"] = df["estimated_hours"].fillna(0)
+        for date_column in ["start_date", "end_date"]:
+            df[date_column] = pd.to_datetime(df[date_column], errors="coerce")
+        df = df[df["end_date"] >= df["start_date"]]
+        df["estimated_hours"] = (
+            pd.to_numeric(df["estimated_hours"], errors="coerce").fillna(0).astype(int)
+        )
+        df = df.drop_duplicates()
+        string_columns = ["client", "project", "role", "name", "task"]
+        for col in string_columns:
+            df[col] = df[col].str.strip().str.title()
+
+        logging.info("Transformed DataFrame:")
+        logging.info(df.head())
+
+        logging.info("Transformed DataFrame:")
+        logging.info(df.head())
+        return df.to_dict()
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise
+
+
+def transform_clickup_data(sheet_data):
+    try:
+        df = pd.DataFrame(sheet_data)
+
+        # transform data
+        df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+        string_columns = ["client", "project", "name", "task", "note"]
+        for col in string_columns:
+            if col in df.columns:
+                df[col] = df[col].str.strip().str.title()
+        if "date" in df.columns:
+            df["date"] = pd.to_datetime(df["date"], errors="coerce")
+            df["date"] = df["date"].dt.strftime("%Y-%m-%dT%H:%M:%S")
+        if "hours" in df.columns:
+            df["hours"] = pd.to_numeric(df["hours"], errors="coerce")
+        if "billable" in df.columns:
+            df["billable"] = (
+                df["billable"].str.strip().str.lower().map({"yes": True, "no": False})
+            )
+        critical_columns = ["client", "project", "name", "task", "date", "hours"]
+        df = df.dropna(subset=critical_columns)
+        if "note" in df.columns:
+            df["note"] = df["note"].fillna("No Note")
+        df = df.drop_duplicates()
+
+        logging.info("Transformed DataFrame:")
+        logging.info(df.head())
+        return df.to_dict()
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise
